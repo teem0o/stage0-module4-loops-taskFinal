4420296cc1b61a4bd3179318cbac85b23f079f89
temo <chxaidzetemo@gmail.com>
2022-07-17 12:35:34 +0400

    powe

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        int s=1;
+        for (int i=0;i<power;i++){
+            s*=numberToPrint;
+        }
+        System.out.println(s);
 
     }
 
